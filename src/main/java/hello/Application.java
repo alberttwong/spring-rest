6cc37c9a1c6edaff61ecaa7fbb4e4053e023cb14
Albert Wong <atwong@alumni.uci.edu>
2019-01-25 16:13:01 -0800

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -68,7 +68,7 @@ public class Application {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Openshift uncontained.io Sample REST API")
+            .title("Albert's Openshift uncontained.io Sample REST API")
             .description("A sample REST API to demonstrate Spring Boot Applications in Openshift.")
             .license("Apache License Version 2.0")
             .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
